a3dcad551cdc3ad1aef9493b7410ff42ceda779c
madmaxWMFU <yasmaks@gmail.com>
2023-10-23 00:32:25 +0300

    added back btn

--- js/script.js
+++ js/script.js
@@ -83,3 +83,17 @@ class Footer extends HTMLElement {
 
 customElements.define("main-header", Header);
 customElements.define("main-footer", Footer);
+
+// Back to top
+const amountScrolled = 200;
+const btnBackToTop = document.querySelector('.back-to-top');
+
+const backToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+const toggleBtnBackToTop = () => {
+  window.scrollY > amountScrolled 
+    ? btnBackToTop.classList.add('back-to-top_show')
+    : btnBackToTop.classList.remove('back-to-top_show');
+}
+
+btnBackToTop.addEventListener('click', backToTop);
+window.addEventListener('scroll', toggleBtnBackToTop);
